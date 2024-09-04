c525a7ca9a2cc14d981e4864d5e1e744fde012c7
josill <josill@taltech.ee>
2024-09-04 12:52:05 +0300

    optional business name migration

--- UptimeTeatmik.Domain/Business.cs
+++ UptimeTeatmik.Domain/Business.cs
@@ -5,6 +5,6 @@ namespace UptimeTeatmik.Domain;
 public class Business : BaseEntityMetadata
 {
     [Key] public Guid BusinessId { get; set; }
-    [MaxLength(256)] public string BusinessName { get; set; } = null!;
+    [MaxLength(256)] public string? BusinessName { get; set; }
     [MaxLength(64)] public string BusinessCode { get; set; } = null!;
 }
