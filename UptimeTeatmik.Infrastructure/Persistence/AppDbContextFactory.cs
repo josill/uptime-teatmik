063ea566fcaf36b7eec66b6c3a977fe04673f14d
josill <josill@taltech.ee>
2024-09-04 11:21:42 +0300

    Updated AppDbContextFactory.cs file to use secrets

--- UptimeTeatmik.Infrastructure/Persistence/AppDbContextFactory.cs
+++ UptimeTeatmik.Infrastructure/Persistence/AppDbContextFactory.cs
@@ -1,16 +1,33 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
 
-namespace UptimeTeatmik.Infrastructure;
+namespace UptimeTeatmik.Infrastructure.Persistence;
 
 public class AppDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
 {
     public AppDbContext CreateDbContext(string[] args)
     {
+        var configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddEnvironmentVariables()
+            .Build();
+
+        var persistenceSettings = new PersistenceSettings();
+        configuration.GetSection(PersistenceSettings.SectionName).Bind(persistenceSettings);
+
+        var host = Environment.GetEnvironmentVariable("DB_HOST") ?? persistenceSettings.Host;
+        var port = Environment.GetEnvironmentVariable("DB_PORT") ?? persistenceSettings.Port.ToString();
+        var username = Environment.GetEnvironmentVariable("DB_USER") ?? persistenceSettings.Username;
+        var password = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? persistenceSettings.Password;
+        var database = Environment.GetEnvironmentVariable("DB_NAME") ?? persistenceSettings.Database;
+
+        var connectionString = $"Host={host};Port={port};Username={username};Password={password};Database={database}";
+   
+        
         var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
-        optionsBuilder.UseNpgsql<AppDbContext>(
-            "Host=localhost;Port=5432;Username=josill;Password=uptime_teatmik;Database=uptime-teatmik"
-        );
+        optionsBuilder.UseNpgsql<AppDbContext>(connectionString);
         
         return new AppDbContext(optionsBuilder.Options);
     }
