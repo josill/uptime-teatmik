a392284e57e43fdca82cea93ebf1f457d89e220c
josill <josill@taltech.ee>
2024-09-05 17:18:34 +0300

    Migrations for computed UniqueCode property on Entity

---
+++ UptimeTeatmik.Infrastructure/Migrations/20240905141520_EntityComputedUniqueCode.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UptimeTeatmik.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class EntityComputedUniqueCode : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "UniqueCode",
+                table: "Entities",
+                type: "character varying(448)",
+                maxLength: 448,
+                nullable: false,
+                computedColumnSql: "COALESCE(\"FirstName\", '') || COALESCE(\"BusinessOrLastName\", '') || \"BusinessOrPersonalCode\"",
+                stored: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UniqueCode",
+                table: "Entities");
+        }
+    }
+}
