cf7214530d24db0ee390f8c9367654abfb794c15
josill <josill@taltech.ee>
2024-09-09 11:07:15 +0300

    SearchForBusinessQueryHandler

---
+++ UptimeTeatmik.Application/Businesses/Queries/SearchForBusinesses/SearchForBusinessQueryHandler.cs
@@ -0,0 +1,19 @@
+using ErrorOr;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using UptimeTeatmik.Application.Common.Interfaces;
+
+namespace UptimeTeatmik.Application.Businesses.Queries.SearchForBusinesses;
+
+public class SearchForBusinessQueryHandler(IAppDbContext dbContext) : IRequestHandler<SearchForBusinessesQuery, ErrorOr<List<BusinessResult>>>
+{
+    public async Task<ErrorOr<List<BusinessResult>>> Handle(SearchForBusinessesQuery request, CancellationToken cancellationToken)
+    {
+        var matchingBusinesses = await dbContext.Entities
+            .Where(e => e.BusinessOrLastName != null && e.BusinessOrLastName.Contains(request.Query))
+            .Select(e => new BusinessResult(e))
+            .ToListAsync(cancellationToken: cancellationToken);
+
+        return matchingBusinesses;
+    }
+}
