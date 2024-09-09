b24ba2429aed3ecbe706a3a5e3e0e7f3440582b5
josill <josill@taltech.ee>
2024-09-09 11:54:55 +0300

    moved BusinessResult into a common folder because it has two references

---
+++ UptimeTeatmik.Application/Businesses/Common/BusinessResult.cs
@@ -0,0 +1,5 @@
+using UptimeTeatmik.Domain;
+
+namespace UptimeTeatmik.Application.Businesses.Common;
+
+public record BusinessResult(Entity Entity);
--- UptimeTeatmik.Application/Businesses/Queries/SearchForBusinesses/SearchForBusinessesQuery.cs
+++ UptimeTeatmik.Application/Businesses/Queries/SearchForBusinesses/SearchForBusinessesQuery.cs
@@ -1,9 +1,8 @@
 using ErrorOr;
 using MediatR;
+using UptimeTeatmik.Application.Businesses.Common;
 using UptimeTeatmik.Domain;
 
 namespace UptimeTeatmik.Application.Businesses.Queries.SearchForBusinesses;
 
 public record SearchForBusinessesQuery(string Query) : IRequest<ErrorOr<List<BusinessResult>>>;
-
-public record BusinessResult(Entity Entity);
