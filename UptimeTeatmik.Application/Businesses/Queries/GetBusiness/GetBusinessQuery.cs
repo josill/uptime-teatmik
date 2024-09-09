c58a18edb04b51078f2f3a37beb217e1ad30f674
josill <josill@taltech.ee>
2024-09-09 11:53:49 +0300

    GetBusinessQuery

---
+++ UptimeTeatmik.Application/Businesses/Queries/GetBusiness/GetBusinessQuery.cs
@@ -0,0 +1,7 @@
+using ErrorOr;
+using MediatR;
+using UptimeTeatmik.Application.Businesses.Queries.SearchForBusinesses;
+
+namespace UptimeTeatmik.Application.Businesses.Queries.GetBusiness;
+
+public record GetBusinessQuery(Guid BusinessId) : IRequest<ErrorOr<BusinessResult>>;
