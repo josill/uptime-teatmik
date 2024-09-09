1847fc130c453547709ed9b8dc36da14d20de30b
josill <josill@taltech.ee>
2024-09-09 15:18:13 +0300

    migrations for optional roles on EntityOwner model

---
+++ UptimeTeatmik.Infrastructure/Migrations/20240909121055_OptionalRoles.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UptimeTeatmik.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class OptionalRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleInEntityAbbreviation",
+                table: "EntityOwners",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleInEntity",
+                table: "EntityOwners",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleInEntityAbbreviation",
+                table: "EntityOwners",
+                type: "character varying(32)",
+                maxLength: 32,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(32)",
+                oldMaxLength: 32,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "RoleInEntity",
+                table: "EntityOwners",
+                type: "character varying(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "character varying(256)",
+                oldMaxLength: 256,
+                oldNullable: true);
+        }
+    }
+}
