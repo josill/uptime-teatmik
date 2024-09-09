51a167e409e7657511b4cffb32795eb898371804
josill <josill@taltech.ee>
2024-09-09 10:56:58 +0300

    SearchForBusinesses endpoint for searching businesses from the frontend

--- UptimeTeatmik.Api/Controllers/BusinessController.cs
+++ UptimeTeatmik.Api/Controllers/BusinessController.cs
@@ -4,6 +4,7 @@ using UptimeTeatmik.Application.Businesses.Queries.UpdatesBusinessesQuery;
 
 namespace UptimeTeatmik.Api.Controllers;
 
+[Route("/v3/businesses")]
 public class BusinessController(ISender mediator) : ApiController
 {
     [HttpGet("updates")]
@@ -11,6 +12,18 @@ public class BusinessController(ISender mediator) : ApiController
     {
         var query = new UpdateBusinessesQuery(date);
         var result = await mediator.Send(query);
+
+        return result.Match(
+            Ok,
+            HandleErrors
+        );
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> SearchForBusinesses([FromQuery] string query)
+    {
+        var mediatorQuery = new SearchForBusinessesQuery(query);
+        var result = await mediator.Send(mediatorQuery);
 
         return result.Match(
             Ok,
