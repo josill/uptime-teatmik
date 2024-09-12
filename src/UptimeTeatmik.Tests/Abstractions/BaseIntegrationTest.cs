9b100ab2aebaadfb3b0f1cd16017a8943798c92a
josill <josill@taltech.ee>
2024-09-12 15:51:22 +0300

    created the BaseIntegrationTest class

--- src/UptimeTeatmik.Tests/Abstractions/BaseIntegrationTest.cs
+++ src/UptimeTeatmik.Tests/Abstractions/BaseIntegrationTest.cs
@@ -1,6 +1,28 @@
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using UptimeTeatmik.Application.Common.Interfaces.BusinessRegisterService;
+using UptimeTeatmik.Infrastructure.Persistence;
+using UptimeTeatmik.Infrastructure.Services.BusinessRegisterService;
+
 namespace UptimeTeatmik.Tests.Abstractions;
 
-public class BaseIntegrationTest
+public class BaseIntegrationTest : IClassFixture<IntegrationTestFactory>, IDisposable
 {
+    private readonly IServiceScope _scope;
+    protected IBusinessRegisterService BusinessRegisterService { get; }
+    protected AppDbContext DbContext { get; }
+    // protected Faker Faker { get; }
+
+    protected BaseIntegrationTest(IntegrationTestFactory factory)
+    {
+        _scope = factory.Services.CreateScope();
+        BusinessRegisterService = _scope.ServiceProvider.GetRequiredService<BusinessRegisterService>();
+        DbContext = _scope.ServiceProvider.GetRequiredService<AppDbContext>();
+        // Faker = new Faker();
+    }
     
+    public void Dispose()
+    {
+        _scope.Dispose();
+    }
 }
