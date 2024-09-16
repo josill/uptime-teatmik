bf77614201a0822567781be93cb9dde3df84fa7e
josill <josill@taltech.ee>
2024-09-16 16:15:02 +0300

    finished subscription logic

--- src/UptimeTeatmik.Application/Businesses/Commands/SubscribeToBusiness/SubscribeToBusinessCommandHandler.cs
+++ src/UptimeTeatmik.Application/Businesses/Commands/SubscribeToBusiness/SubscribeToBusinessCommandHandler.cs
@@ -1,5 +1,6 @@
 using ErrorOr;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 using UptimeTeatmik.Application.Common.Interfaces;
 using UptimeTeatmik.Application.Common.Interfaces.BusinessRegisterService;
 using UptimeTeatmik.Domain.Errors;
@@ -13,16 +14,39 @@ public class SubscribeToBusinessCommandHandler(IAppDbContext dbContext, IBusines
     {
         var business = await businessRegisterService.UpdateBusinessAsync(command.BusinessCode);
         if (business == null) return Errors.Business.FailureGettingBusiness(command.BusinessCode);
-
-        var subscription = new Subscription()
-        {
-            SubscribedBusinessId = business.Id,
-            SubscribersEmail = command.SubscribersEmail,
-            EventTypes = command.EventTypes ?? [],
-            UpdateParameters = command.UpdateParameters ?? [],
-        };
         
         // TODO: Check for existing subscription or create one 
-        return new Success();
+        var existingSubscription = await dbContext.Subscriptions
+            .FirstOrDefaultAsync(s => s.SubscribedBusinessId == business.Id, cancellationToken: cancellationToken);
+        if (existingSubscription == null)
+        {
+            var subscription = new Subscription()
+            {
+                SubscribedBusinessId = business.Id,
+                SubscribersEmail = command.SubscribersEmail,
+                EventTypes = command.EventTypes ?? [],
+                UpdateParameters = command.UpdateParameters ?? [],
+            };
+            dbContext.Subscriptions.Add(subscription);
+        }
+        else
+        {
+            if (command.EventTypes != null)
+            {
+                existingSubscription.EventTypes = existingSubscription.EventTypes
+                    .Union(command.EventTypes)
+                    .ToList();
+            }
+
+            if (command.UpdateParameters != null)
+            {
+                existingSubscription.UpdateParameters = existingSubscription.UpdateParameters
+                    .Union(command.UpdateParameters)
+                    .ToList();
+            }
+        }
+        await dbContext.SaveChangesAsync(cancellationToken);
+
+        return Result.Success;
     }
 }
