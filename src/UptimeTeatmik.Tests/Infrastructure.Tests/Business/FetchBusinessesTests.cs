8f8f4028d0c01a2f0b57d9286c673a828a8fc363
josill <josill@taltech.ee>
2024-09-16 11:38:47 +0300

    ShouldNot_FetchUpdatedBusinesses_WhenDateIsInvalid and ShouldNot_FetchUpdatedBusinesses_WhenDateIsMissing tests

--- src/UptimeTeatmik.Tests/Infrastructure.Tests/Business/FetchBusinessesTests.cs
+++ src/UptimeTeatmik.Tests/Infrastructure.Tests/Business/FetchBusinessesTests.cs
@@ -16,7 +16,6 @@ public class FetchBusinessesTests(IntegrationTestWebAppFactory factory, ITestOut
         var isoDate = date.ToString("o");
 
         // Act
-        // var updateResponse = await HttpClient.PostAsJsonAsync($"v1/businesses/update", request);
         var updateResponse = await HttpClient.GetAsync($"v1/businesses/updates?date={isoDate}");
 
         // Assert
@@ -24,8 +23,32 @@ public class FetchBusinessesTests(IntegrationTestWebAppFactory factory, ITestOut
         var updateResponseData = Newtonsoft.Json.JsonConvert.DeserializeObject<UpdateBusinessesResult>(updateResponseBody);
         updateResponse.StatusCode.Should().Be(HttpStatusCode.OK);
         
-        testOutputHelper.WriteLine(updateResponseBody);
         Assert.NotNull(updateResponseData);
         Assert.Equal(162, updateResponseData.AmountOfBusinessesUpdated);
+    }
+    
+    [Fact]
+    public async Task ShouldNot_FetchUpdatedBusinesses_WhenDateIsMissing()
+    {
+        // Arrange
+
+        // Act
+        var updateResponse = await HttpClient.GetAsync($"v1/businesses/updates");
+
+        // Assert
+        updateResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+    }
+    
+    [Fact]
+    public async Task ShouldNot_FetchUpdatedBusinesses_WhenDateIsInvalid()
+    {
+        // Arrange
+        var date = new DateTime(2000, 6, 30);
+
+        // Act
+        var updateResponse = await HttpClient.GetAsync($"v1/businesses/updates?date={date}");
+
+        // Assert
+        updateResponse.StatusCode.Should().Be(HttpStatusCode.BadRequest);
     }
 }
