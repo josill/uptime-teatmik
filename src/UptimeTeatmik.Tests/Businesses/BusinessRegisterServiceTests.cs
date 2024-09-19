829e4f1a4e3dc89b4a49a05a180c7a597c980bfa
josill <josill@taltech.ee>
2024-09-19 15:19:49 +0300

    update businesses test for hangfire methods

--- src/UptimeTeatmik.Tests/Businesses/BusinessRegisterServiceTests.cs
+++ src/UptimeTeatmik.Tests/Businesses/BusinessRegisterServiceTests.cs
@@ -1,6 +1,6 @@
 using Hangfire;
+using Hangfire.Common;
 using Hangfire.States;
-using Microsoft.EntityFrameworkCore;
 using UptimeTeatmik.Tests.Businesses.Common;
 using Xunit.Abstractions;
 
@@ -18,7 +18,7 @@ public class BusinessRegisterServiceTests : IClassFixture<TestFactory>
     }
 
     [Fact]
-    public async Task CreateEntity_ShouldSucceed_WhenEntityDoesNotExist()
+    public async Task CreateUpdateBusinessJob_ShouldBeEnqueued_WhenCreatingJob()
     {
         // Arrange
         var testBusinessCode = "10308733";
@@ -26,59 +26,17 @@ public class BusinessRegisterServiceTests : IClassFixture<TestFactory>
 
         // Act
         await _testFactory.BusinessRegisterService.UpdateBusinessesAsync(businessCodes);
-        // await Task.Delay(10000);
-        var jobCreated = await CheckIfJobWasCreated(testBusinessCode);
-        await WaitForJobCompletion(testBusinessCode, TimeSpan.FromSeconds(10));
-        var createdBusiness = await _testFactory.DbContext.Entities
-            .FirstOrDefaultAsync();
-            // .FirstOrDefaultAsync(e => e.BusinessOrPersonalCode == testBusinessCode);
+        var jobCreated = await CheckIfJobWasCreated();
 
-
-            
         // Assert
-        Assert.True(jobCreated, "Expected background job was not created");
-        // Assert.NotNull(createdBusiness);
-        // Assert.Single(_testFactory.DbContext.Entities);
-        // Assert.Equal(createdBusiness.BusinessOrPersonalCode, createdBusiness.BusinessOrPersonalCode);
-
-        // You may want to add assertions here to check if the background job was enqueued or executed
+        Assert.True(jobCreated != null, "Expected background job was not created");
     }
     
-    public async Task<bool> CheckIfJobWasCreated(string businessCode)
+    public async Task<Job?> CheckIfJobWasCreated()
     {
         var jobs = JobStorage.Current.GetMonitoringApi();
-        var enqueuedCount = jobs.EnqueuedCount(EnqueuedState.DefaultQueue);
-        var servers = jobs.Servers();
-        _testOutputHelper.WriteLine($"Number of servers: {servers.Count}");
-        _testOutputHelper.WriteLine(enqueuedCount.ToString());
-        return enqueuedCount > 0;
-    }
-    
-    private async Task WaitForJobCompletion(string jobId, TimeSpan timeout, int delayInMs = 500)
-    {
-        var jobs = JobStorage.Current.GetMonitoringApi();
+        var enqueuedCount = jobs.EnqueuedJobs(EnqueuedState.DefaultQueue, 0, 1);
 
-        var startTime = DateTime.UtcNow;
-        while (DateTime.UtcNow - startTime < timeout)
-        {
-            var enqueued = jobs.EnqueuedJobs(EnqueuedState.DefaultQueue, 0, 50);
-            var job = enqueued.FirstOrDefault();
-            _testOutputHelper.WriteLine(job.ToString());
-            var state = job.Value.State;
-            _testOutputHelper.WriteLine("state");
-            _testOutputHelper.WriteLine(state);
-            
-            switch (state)
-            {
-                case "Succeeded":
-                    return;
-                case "Failed":
-                    throw new Exception($"Job {jobId} failed");
-                default:
-                    await Task.Delay(delayInMs); 
-                    break;
-            }
-        }
-        throw new TimeoutException($"Job {jobId} did not complete within the specified timeout");
+        return enqueuedCount.FirstOrDefault().Value.Job;
     }
 }
